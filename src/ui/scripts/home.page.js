8e967d8575d883a7f3a029e56c9755c997c33dcd
ssaavedraa <ssaavedraa@santiagosaavedra.com.co>
2026-09-04 17:35:54 -0500

    chore(logs): remove unnecessary log statement

--- src/ui/scripts/home.page.js
+++ src/ui/scripts/home.page.js
@@ -1,7 +1,5 @@
 import { renderLayoutComponent } from './layout.js'
 
-console.log('mi propia URL es: ', import.meta.url)
-
 const components = [
   renderLayoutComponent('../components/header.html', 'header-slot'),
   renderLayoutComponent('../components/footer.html', 'footer-slot')
