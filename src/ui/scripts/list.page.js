bb98fc0e780a8e1615f9e83b13105d54f47dc5d5
ssaavedraa <ssaavedraa@santiagosaavedra.com.co>
2026-09-05 13:21:47 -0500

    feat(list): implement list page functionality with empty state handling and post rendering

---
+++ src/ui/scripts/list.page.js
@@ -0,0 +1,49 @@
+import { filterNotDeleted, sortByDate } from '../../domain/posts.js'
+import { HttpClient } from '../../services/http-client.service.js'
+import { renderCard } from './card.component.js'
+
+const gridSlot = document.getElementById('news-hub-section')
+
+if (!gridSlot) {
+  throw new Error('Grid slot with ID "news-hub-section" not found.')
+}
+
+const emptyStateElement = document.getElementById('empty-state')
+
+if (!emptyStateElement) {
+  throw new Error('No empty state section found')
+}
+
+const postsUrl = new URL('../../assets/data/posts.json', import.meta.url).href
+const postsData = await HttpClient.get(postsUrl, 'application/json')
+const activePosts = filterNotDeleted(postsData)
+
+if (!activePosts || activePosts.length === 0) {
+  emptyStateElement.classList.remove('empty-state--hidden')
+
+  const emptyStateContent = `
+    <div class="empty-list__container">
+      <p class="empty-list__title"> There are no posts </p>
+      <a class="button button--accent" href="src/ui/pages/create.html"> Create one </a>
+    </div>
+  `
+
+  emptyStateElement.innerHTML = emptyStateContent
+} else {
+  emptyStateElement.classList.add('empty-state--hidden')
+  const sortedPosts = sortByDate(activePosts)
+
+  sortedPosts.forEach((post) => {
+    const postElement = renderCard({
+      title: post.title,
+      summary: post.summary,
+      image: {
+        src: post.image.src,
+        alt: post.image.alt,
+      },
+      slug: post.slug,
+    })
+
+    gridSlot.insertAdjacentHTML('beforeend', postElement)
+  })
+}
