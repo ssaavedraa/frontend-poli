897a77e261b89cab3f1a262b4d5008205379d771
ssaavedraa <ssaavedraa@santiagosaavedra.com.co>
2026-09-05 10:36:42 -0500

    feat(detail): implement post retrieval and rendering logic in detail page

---
+++ src/domain/detail.js
@@ -0,0 +1,3 @@
+export function findPostBySlug(posts, slug) {
+  return posts.find(post => post.slug === slug)
+}
---
+++ src/ui/scripts/detail.page.js
@@ -0,0 +1,34 @@
+import { findPostBySlug } from '../../domain/detail.js'
+import { HttpClient } from '../../services/http-client.service.js'
+
+const postSlug = new URLSearchParams(window.location.search).get('slug')
+
+const posts = await HttpClient.get(new URL('../../assets/data/posts.json', import.meta.url).href, 'application/json')
+
+const post = findPostBySlug(posts, postSlug)
+
+if (!post) {
+  throw new Error(`Post with slug "${postSlug}" not found`)
+}
+
+const newsImageElement = document.getElementById('news-image')
+const newsTitleElement = document.getElementById('news-title')
+const newsSubtitleElement = document.getElementById('news-subtitle')
+const newsContentElement = document.getElementById('news-content')
+
+if (!newsImageElement || !newsTitleElement || !newsSubtitleElement || !newsContentElement) {
+  throw new Error('One or more required elements are missing in the DOM')
+}
+
+newsImageElement.setAttribute('src', post.image.src)
+newsImageElement.setAttribute('alt', `${post.image.alt}`)
+newsTitleElement.textContent = post.title
+newsSubtitleElement.textContent = post.subtitle
+
+const newsContent = post.content
+
+newsContent.forEach((contentItem) => {
+  const contentElement = document.createElement('p')
+  contentElement.textContent = contentItem
+  newsContentElement.appendChild(contentElement)
+})
