d176ba5f55315ff9bf74f5503de87eca6aa15292
ssaavedraa <ssaavedraa@santiagosaavedra.com.co>
2026-09-05 19:17:48 -0500

    feat(favorites): implement favorite button functionality and styles

--- src/ui/scripts/detail.page.js
+++ src/ui/scripts/detail.page.js
@@ -1,4 +1,6 @@
 import { findPostBySlug } from '../../domain/detail.js'
+import { isFavorited } from '../../domain/favorites.js'
+import { FavoritesService } from '../../services/favorites.service.js'
 import { HttpClient } from '../../services/http-client.service.js'
 
 const postSlug = new URLSearchParams(window.location.search).get('slug')
@@ -11,12 +13,12 @@ const pageTitle = postSlug.split('-').map(word => word.charAt(0).toUpperCase() +
 document.title = `NewsHub | ${pageTitle}`
 
 const posts = await HttpClient.get(new URL('../../assets/data/posts.json', import.meta.url).href, 'application/json')
-
 const post = findPostBySlug(posts, postSlug)
-
 if (!post) {
   throw new Error(`Post with slug "${postSlug}" not found`)
 }
+
+const favorites = FavoritesService.getFavorites()
 
 const newsImageElement = document.getElementById('news-image')
 const newsTitleElement = document.getElementById('news-title')
@@ -38,4 +40,43 @@ newsContent.forEach((contentItem) => {
   const contentElement = document.createElement('p')
   contentElement.textContent = contentItem
   newsContentElement.appendChild(contentElement)
+})
+
+let isFavoriteButtonActive = isFavorited(post.id, favorites)
+const favoritesButton = document.getElementById('favorites-button')
+
+if (!favoritesButton) {
+  throw new Error('Favorites button not found')
+}
+
+function addFavoriteStyles() {
+  favoritesButton.innerText = 'Remove from favorites'
+  favoritesButton.classList.add('button--active')
+}
+
+function removeFavoriteStyles() {
+  favoritesButton.innerText = 'Add to favorites'
+  favoritesButton.classList.remove('button--active')
+}
+
+function toggleFavorite(id) {
+  if (isFavoriteButtonActive) {
+    FavoritesService.removeFavorite(id)
+    removeFavoriteStyles()
+  } else {
+    FavoritesService.addFavorite(id)
+    addFavoriteStyles()
+  }
+
+  isFavoriteButtonActive = !isFavoriteButtonActive
+}
+
+if (isFavoriteButtonActive) {
+  addFavoriteStyles()
+} else {
+  removeFavoriteStyles()
+}
+
+favoritesButton.addEventListener('click', () => {
+  toggleFavorite(post.id)
 })
