765b1dde4db62eef0eb38fa1b5c46f1c8f94c0ea
ssaavedraa <ssaavedraa@santiagosaavedra.com.co>
2026-09-19 01:54:14 -0500

    feat(header): implement standalone header component with navigation links and styles

--- angular/src/app/app.ts
+++ angular/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {}
