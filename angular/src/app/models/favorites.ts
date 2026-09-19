a491196c1c4a43927726399b2e31b565efbd39e4
ssaavedraa <ssaavedraa@santiagosaavedra.com.co>
2026-09-19 00:22:32 -0500

    feat(models): add TypeScript interfaces for contact form, favorites, images, and posts to define data structures

---
+++ angular/src/app/models/favorites.ts
@@ -0,0 +1,1 @@
+type Favorites = string[]
---
+++ angular/src/app/models/post.ts
@@ -0,0 +1,12 @@
+interface Post {
+  id: string
+  title: string
+  slug: string
+  subtitle: string
+  summary: string
+  content: string[]
+  image: Image
+  createdAt: Date
+  updatedAt: Date
+  deletedAt?: Date
+}
