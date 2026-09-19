6fe29a87c0a0278b20cbaaba73116ce77261716e
ssaavedraa <ssaavedraa@santiagosaavedra.com.co>
2026-09-19 02:36:45 -0500

    feat(routes): define initial application routes including home page component for navigation

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home/home.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'NewsHub | Home',
+    component: HomePage,
+  },
+  // {
+  //   path: 'favorites',
+  //   title: 'NewsHub | Favorites',
+  //   component: FavoritesPage,
+  // },
+  // {
+  //   path: 'post/:slug',
+  //   title: 'NewsHub | Post',
+  //   component: PostPage,
+  // },
+  // {
+  //   path: 'create',
+  //   title: 'NewsHub | Create Post',
+  //   component: CreatePostPage,
+  // },
+  // {
+  //   path: 'contact',
+  //   title: 'NewsHub | Contact',
+  //   component: ContactPage,
+  // },
+  // {
+  //   path: 'list',
+  //   title: 'NewsHub | List',
+  //   component: ListPage,
+  // },
+  // {
+  //   path: '**',
+  //   title: 'NewsHub | Not Found',
+  //   component: NotFoundPage,
+  // },
+];
