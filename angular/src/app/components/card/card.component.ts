3501d0628f0655537a5815faf360b6e4ff4224cc
ssaavedraa <ssaavedraa@santiagosaavedra.com.co>
2026-09-19 02:36:07 -0500

    fix(card): update post input type to CardPost for consistency with new model structure

--- angular/src/app/components/card/card.component.ts
+++ angular/src/app/components/card/card.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Post } from '../../models';
+import { CardPost } from '../../models';
 
 @Component({
   selector: 'app-card',
@@ -10,5 +10,5 @@ import { Post } from '../../models';
   styleUrl: './card.component.css',
 })
 export class CardComponent {
-  @Input({ required: true}) post!: Pick<Post, 'image' | 'title' | 'summary' | 'slug'>;
+  @Input({ required: true}) post!: CardPost
 }
