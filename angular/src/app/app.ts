88aa119e886b6c493a68522803545881b86c26db
ssaavedraa <ssaavedraa@santiagosaavedra.com.co>
2026-09-19 01:57:11 -0500

    feat(footer): add standalone footer component with navigation links and styles, and integrate into app

--- angular/src/app/app.ts
+++ angular/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
 })
 export class App {}
