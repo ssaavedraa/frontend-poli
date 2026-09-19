2b4309fb1ef28892e3dae7aa6c20573e317b3daa
ssaavedraa <ssaavedraa@santiagosaavedra.com.co>
2026-09-18 20:34:58 -0500

    feat(posts): add functions to get latest slug index and slugify text

--- src/domain/posts.js
+++ src/domain/posts.js
@@ -9,3 +9,20 @@ export function sortByDate(posts) {
 export function truncateAt(posts, count) {
   return posts.slice(0, count)
 }
+
+export function getLatestSlugIndex(slug, storedPosts) {
+  const slugRegex = new RegExp(`^${slug}$|^${slug}-[0-9]+$`)
+
+  const matchCount = storedPosts
+    .filter((post) => slugRegex.test(post.slug)).length
+
+  return matchCount
+}
+
+export function slugify(text) {
+  return text.normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/^-+|-+$/g, '')
+}
