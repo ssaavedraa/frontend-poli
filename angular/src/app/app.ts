f0db302d3ebd03dc2240828e8c7afb5f7211dc89
ssaavedraa <ssaavedraa@santiagosaavedra.com.co>
2026-09-19 02:12:20 -0500

    refactor(app): consolidate component imports in app.ts for improved organization

--- angular/src/app/app.ts
+++ angular/src/app/app.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components';
-import { FooterComponent } from './components/footer/footer.component';
+import { FooterComponent, HeaderComponent } from './components';
 
 @Component({
   selector: 'app-root',
