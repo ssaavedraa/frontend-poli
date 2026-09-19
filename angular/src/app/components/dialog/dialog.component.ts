33be4ee14200e1355ffaf1a64ddc5305a2a7a802
ssaavedraa <ssaavedraa@santiagosaavedra.com.co>
2026-09-19 12:48:04 -0500

    feat(dialog): implement dialog component with HTML, CSS, and TypeScript for user confirmation; includes responsive design and event handling

---
+++ angular/src/app/components/dialog/dialog.component.ts
@@ -0,0 +1,33 @@
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core'
+
+@Component({
+  selector: 'app-dialog',
+  standalone: true,
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.css'],
+})
+export class DialogComponent {
+  @Input() title: string = ''
+  @Input() message: string = ''
+  @Output() confirm = new EventEmitter<void>()
+  @Output() cancel = new EventEmitter<void>()
+  @ViewChild('appDialog') dialogRef: ElementRef<HTMLDialogElement> | undefined = undefined
+
+  openDialog(): void {
+    if (!this.dialogRef) {
+      console.warn('Dialog reference not found')
+      return
+    }
+
+    this.dialogRef.nativeElement.showModal()
+  }
+
+  closeDialog(): void {
+    if (!this.dialogRef) {
+      console.warn('Dialog reference not found')
+      return
+    }
+
+    this.dialogRef.nativeElement.close()
+  }
+}
