f4808b064f920af000992c5bc3918a9e23701894
ssaavedraa <ssaavedraa@santiagosaavedra.com.co>
2026-09-19 11:54:33 -0500

    fix(detail): update findPostBySlug function to return null instead of undefined when no post is found

--- angular/src/app/domain/detail.ts
+++ angular/src/app/domain/detail.ts
@@ -1,5 +1,5 @@
 import { Post } from '../models/post';
 
-export function findPostBySlug(posts: Post[], slug: string): Post | undefined {
-  return posts.find(post => post.slug === slug && !post.deletedAt)
+export function findPostBySlug(posts: Post[], slug: string): Post | null {
+  return posts.find(post => post.slug === slug && !post.deletedAt) ?? null
 }
