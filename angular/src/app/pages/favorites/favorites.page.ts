1de8dbef47df636f89f15cbbb24bf66fd7b3c2de
ssaavedraa <ssaavedraa@santiagosaavedra.com.co>
2026-09-20 19:55:06 -0500

    style(favorites): clean up HTML formatting and ensure consistent syntax in favorites page

--- angular/src/app/pages/favorites/favorites.page.ts
+++ angular/src/app/pages/favorites/favorites.page.ts
@@ -1,14 +1,14 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { CardComponent } from '../../components';
-import { filterNotDeleted, getFavoritesData, sortByDate } from '../../domain';
-import { CardPost } from '../../models';
-import { FavoritesService, PostsService } from '../../services';
+import { Component, inject, OnInit } from '@angular/core'
+import { CardComponent } from '../../components'
+import { filterNotDeleted, getFavoritesData, sortByDate } from '../../domain'
+import { CardPost } from '../../models'
+import { FavoritesService, PostsService } from '../../services'
 
 @Component({
   selector: 'app-favorites-page',
   imports: [CardComponent],
   templateUrl: './favorites.page.html',
-  styleUrls: ['./favorites.page.css']
+  styleUrls: ['./favorites.page.css'],
 })
 export class FavoritesPage implements OnInit {
   private readonly postsService = inject(PostsService)
